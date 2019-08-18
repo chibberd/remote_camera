925e995f0e7ba00999fa148076620127dc6c95e9
chibberd <hibberd_chris@hotmail.com>
2019-08-18 22:45:55 +0100

    WIP: Placeholder examples for sensors.

--- birdbox.py
+++ birdbox.py
@@ -4,10 +4,28 @@ import gpiozero
 import flask
 import uuid
 import threading
+import LIS3DH
+
 
 app = flask.Flask(__name__)
 
 
+def read_pir():
+    pir = gpiozero.DigitalInputDevice(5)
+    pir.value()
+
+def read_accelerometer():
+    accel = LIS3DH.Accelerometer('spi', i2cAddress = 0x0, spiPort = 0, spiCS = 0)  # spi connection alternative
+    accel.set_ODR(odr=50, powerMode='normal')
+    accel.axis_enable(x='on',y='on',z='on')
+    accel.interrupt_high_low('high')
+    accel.latch_interrupt('on')
+    accel.set_BDU('on')
+    accel.set_scale()
+    x = accel.x_axis_reading()
+    y = accel.y_axis_reading()
+    z = accel.z_axis_reading()
+                   
 def worker():
     pir = gpiozero.DigitalInputDevice(5)
     counter = 1
